5eb20922419a4085212bfd627f5cd071c83f210b
Vincent Loh <vincent.ml@gmail.com>
2020-12-23 21:12:06 +0800

    enable CSS Modules with SASS

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,4 +1,8 @@
+import styles from './styles.module.scss'
+
 const Button: React.FC = () => (
-  <button className="text-white bg-blue-400 p-2 rounded">Click Me</button>
+  <button className={styles.root + ' text-white bg-blue-400 p-2 rounded'}>
+    Next.js 10 + TypeScript + Tailwind + SASS Modules + Prettier starter kit
+  </button>
 )
 export default Button
