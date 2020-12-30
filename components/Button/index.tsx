9152a719f0524c384cc5c27a268c3b8e5af955e2
Vincent Loh <vincent.ml@gmail.com>
2020-12-30 20:34:43 +0800

    add eslint, eslint react hooks, pre-commit lint step

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react'
 import styles from './styles.module.scss'
 
-const Button: React.FC = () => (
-  <button className={styles.root + ' text-white bg-blue-400 p-2 rounded'}>
-    Next.js 10 + TypeScript + Tailwind + SASS Modules + Prettier starter kit
-  </button>
-)
+const Button = (a: string) => {
+  useEffect(() => {
+    console.log(`a:`, a)
+  }, [a]) // remove a to trigger eslint error
+  return (
+    <button className={styles.root + ' text-white bg-blue-400 p-2 rounded'}>
+      Next.js 10 + TypeScript + Tailwind + SASS Modules + Prettier starter kit
+    </button>
+  )
+}
 export default Button
